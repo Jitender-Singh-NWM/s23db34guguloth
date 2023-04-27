05a86c0945771483b0f4ab8862fd532ba74c13df
Jitender-Singh-NWM <jithu.cbit@gmail.com>
2023-04-26 20:18:21 -0500

    Pug update

--- models/Dog.js
+++ models/Dog.js
@@ -2,7 +2,14 @@ const mongoose = require("mongoose")
 const dogSchema = mongoose.Schema({
 Breed: String,
 Dimension: String,
-value: Number
-})
+value: {
+    type:Number,
+    set: v => Math.floor(v),
+    
+    min: [0, 'value must be at least 1'],
+    max:[1000,'value must be less than 10']
+    
+}}
+)
 module.exports = mongoose.model("Dog",
 dogSchema)
