e93fc4e72cb7ff138fd17c403ffa14f7eb83e0f7
Jitender-Singh-NWM <jithu.cbit@gmail.com>
2023-04-26 20:48:18 -0500

    Dog file update

--- routes/Dog.js
+++ routes/Dog.js
@@ -1,15 +1,22 @@
 var express = require('express');
 const dog_controlers= require('../controllers/dog');
 var router = express.Router();
+const secured = (req, res, next) => {
+  if (req.user){
+  return next();
+  }
+  req.session.returnTo = req.originalUrl;
+  res.redirect("/login");
+  }
 /* GET costumes */
 router.get('/', dog_controlers.dog_view_all_Page );
 /* GET detail costume page */
 router.get('/detail', dog_controlers.dog_view_one_Page);
 /* GET create costume page */
-router.get('/create', dog_controlers.dog_create_Page);
+router.get('/create', secured, dog_controlers.dog_create_Page);
 
-/* GET create update page */
-router.get('/update', dog_controlers.dog_update_Page);
+/* GET  update page */
+router.get('/update', secured, dog_controlers.dog_update_Page);
 /* GET delete costume page */
-router.get('/delete', dog_controlers.dog_delete_Page);
+router.get('/delete',secured, dog_controlers.dog_delete_Page);
 module.exports = router;
